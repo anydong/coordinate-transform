101de6a8112965278e44afbe8ed459baeed26916
Your Name <you@example.com>
2025-02-10 16:05:00 +0800

    feat: distance

---
+++ src/distance.ts
@@ -0,0 +1,5 @@
+import { distance } from '@turf/distance'
+import type { Coord, Units } from '@turf/helpers';
+
+
+export { distance, type Coord, type Units }
---
+++ tests/distance.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+import { distance, gcj02towgs84 } from '../src/index';
+
+
+describe("distance", () => {
+    it("gcj-02", () => {
+        const a = [116.368904, 39.923423];
+        const b = [116.387271, 39.922501];
+
+        const a_wgs84 = gcj02towgs84(a[0], a[1]);
+        const b_wgs84 = gcj02towgs84(b[0], b[1]);
+        const result = distance(a_wgs84, b_wgs84, { units: "meters" })
+
+        console.log("a_wgs84", a_wgs84)
+        console.log("b_wgs84", b_wgs84)
+        console.log(result)
+
+    })
+})
