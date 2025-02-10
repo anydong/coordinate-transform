f4af887c11a1e0c8064c475d47b6675e68251f3f
Your Name <you@example.com>
2025-02-10 11:22:12 +0800

    feat: dts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,49 @@
-import { wgs84togcj02, gcj02towgs84, gcj02tobd09, bd09togcj02 } from 'coordtransform'
+import coordtransform from 'coordtransform'
+
+
+/**
+ * WGS-84 转 GCJ-02
+ * 
+ * @param lng 经度
+ * @param lat 维度
+ * @returns [lng, lat]
+ */
+const wgs84togcj02 = (lng: number, lat: number): [number, number] => {
+    return coordtransform.wgs84togcj02(lng, lat);
+}
+
+/**
+ * GCJ-02 转 WGS-84
+ * 
+ * @param lng 经度
+ * @param lat 维度
+ * @returns [lng, lat]
+ */
+const gcj02towgs84 = (lng: number, lat: number): [number, number] => {
+    return coordtransform.gcj02towgs84(lng, lat);
+}
+
+/**
+ * GCJ-02 转 BD-09
+ * 
+ * @param lng 经度
+ * @param lat 维度
+ * @returns [lng, lat]
+ */
+const gcj02tobd09 = (lng: number, lat: number): [number, number] => {
+    return coordtransform.gcj02tobd09(lng, lat);
+}
+
+/**
+ * BD-09 转 GCJ-02
+ * 
+ * @param lng 经度
+ * @param lat 维度
+ * @returns [lng, lat]
+ */
+const bd09togcj02 = (lng: number, lat: number): [number, number] => {
+    return coordtransform.bd09togcj02(lng, lat);
+}
 
 /**
  * WGS-84 转换为 BD-09
